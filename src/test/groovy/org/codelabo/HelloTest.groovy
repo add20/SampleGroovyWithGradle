84d252b80233a5519d5bed925de615000dbc84f7
add20 <add20g@gmail.com>
2013-01-27 14:40:13 +0900

    Add setUp function

--- src/test/groovy/org/codelabo/HelloTest.groovy
+++ src/test/groovy/org/codelabo/HelloTest.groovy
@@ -4,7 +4,13 @@ import groovy.util.GroovyTestCase
 import org.codelabo.Hello
 
 class HelloTest extends GroovyTestCase {
+  def hello
+
+  void setUp() {
+    hello = "Hello, World!"
+  }
+
   void testHello() {
-    assert new Hello().hello == "Hello, World!"
+    assert new Hello().hello == hello
   }
 }
